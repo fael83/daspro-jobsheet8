a01449e1c59aca5fde533b6efa7b6aae11c5b670
fael83 <rafaellokajaya@gmail.com>
2024-11-08 09:59:53 +0700

    Experiment1, Question2

--- Star13.java
+++ Star13.java
@@ -5,7 +5,7 @@ public class Star13 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter value N = ");
         int N = sc.nextInt();
-        for (int i = 0; i <= N; i++){
+        for (int i = 1; i > N; i++){
             System.out.print("*");
         }
     }
